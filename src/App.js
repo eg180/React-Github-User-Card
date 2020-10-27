143b6156692cfc5195d5f2212a2dc1a17ce5f45f
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-27 16:49:19 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+import axios from 'axios';
+
+class App extends React.Component {
+
+
+  componentDidMount() {
+    console.log('hey');
+  }
+
+  render() {
+    return (<h1>Testing</h1>);
+  }
+}
+
+export default App;
