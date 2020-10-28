76383c1b8d1c138845e8aa16cf32395c8e814fb8
Erick Gonzalez <ericksinbox@gmail.com>
2020-10-27 23:48:05 -0400

    Added search functionality to find github user by username

--- src/App.js
+++ src/App.js
@@ -3,28 +3,26 @@ import './App.css';
 import axios from 'axios';
 import UserCard from './UserCard';
 import Header from './Header';
+import Search from './Search';
 
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      name: "",
-      user: ""
+      userdata: [],
+      currentUser: "eg180"
     }
    
   }
-
 
   componentDidMount() {
     console.log('inside componentDidMount');
     axios.get('https://api.github.com/users/eg180')
     .then(res => {
+      console.log(res.data);
       this.setState({
-         name: res.data.name,
-         user: res.data.user,
-         pictureurl: res.data.avatar_url,
-         followers: res.data.followers
+         userdata: res.data
         })
     })
     .catch(err => {
@@ -32,11 +30,32 @@ class App extends React.Component {
     })
   };
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.currentUser !== this.state.currentUser) {
+      axios.get(`https://api.github.com/users/${this.state.currentUser}`)
+      .then(res => {
+        this.setState({
+           userdata: res.data
+          })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+    }
+  }
+
+  updateStateForSearch = (newUser) => {
+    this.setState({
+      currentUser: newUser
+    });
+  }
+
   render() {
     return (
       <>
       <Header />
-      <UserCard followers={this.state.followers} avatar={this.state.pictureurl} name={this.state.name}/>
+      <Search newSearch ={this.updateStateForSearch} />
+      <UserCard public_repos={this.state.userdata.public_repos} location={this.state.userdata.location} login={this.state.userdata.login} followers={this.state.userdata.followers} avatar={this.state.userdata.avatar_url} name={this.state.userdata.name}/>
       </>
       )
   }
